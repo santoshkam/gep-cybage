53687ca8aff72db650a9e7931b10ec708ad55620
santosh <santoshkam@cybage.com>
2013-04-22 21:12:21 +0530

    "first commit."

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CybageDemo::Application.config.secret_token = '4d379704bc7ad4c6e0a8aab4d2d28f742c0e90a0829502c6041c919fa3945690631e9177411599daa696a028c5c4caac20b0d1f27a7fd03d7ae75a74500d0b0c'
